a344d0732c31fe55860e88829e00c21ed51b20c3
Albrecht Köhnlein <16088567+koehnlein@users.noreply.github.com>
2025-01-29 17:11:23 +0100

    [TASK] Raise dependencies to allow installation with TYPO3 13 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Ecodev',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '10.4.0-13.4.99',
         ],
     ],
 ];
